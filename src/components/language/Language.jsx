435c72da2d2c8aeeb4a5e5d87a4ce830063109a3
Delvin Lim <tkjpublic@gmail.com>
2023-02-21 00:38:51 +0700

    fix bug minor

--- src/components/language/Language.jsx
+++ src/components/language/Language.jsx
@@ -1,25 +1,22 @@
 import React, { useState, useEffect } from "react";
 import { useTranslation } from "react-i18next";
 import i18next from "i18next";
-import { Button, List, ListItem, ListSubheader, Popover } from "@mui/material";
-import { ArrowDropDown } from "@mui/icons-material";
-import { LanguageContainer, LanguageText, LanguageWrapper, ListContainer, Option, Select } from "./Language.element";
+import {
+  LanguageContainer,
+  LanguageWrapper,
+  Option,
+  Select,
+} from "./Language.element";
 
 const languageMap = {
   en: { label: "EN", dir: "ltr", active: true },
   in: { label: "IN", dir: "ltr", active: false },
 };
-// zh: { label: "ZH", dir: "ltr", active: false }
 
 const Language = () => {
-  const selected = localStorage.getItem("i18nextLng").split('-')[0] || "en";
+  const selected = localStorage.getItem("i18nextLng").split("-")[0] || "en";
   // const selected = localStorage.getItem("i18nextLng").toLowerCase() || "en";
   const { t } = useTranslation();
-
-  // const [menuAnchor, setMenuAnchor] = useState(null);
-  // useEffect(() => {
-  //   document.body.dir = languageMap[selected].dir;
-  // }, [menuAnchor, selected]);
 
   useEffect(() => {
     document.body.dir = languageMap[selected].dir;
@@ -29,19 +26,17 @@ const Language = () => {
     const value = e.target.value;
     i18next.changeLanguage(value);
   };
-  
+
   return (
-      <LanguageContainer>
+    <LanguageContainer>
       <LanguageWrapper>
         <Select onChange={handleFilters} value={selected}>
           {/* <Option disabled select>
           {t("select_language")}
           </Option>  */}
-          {Object.keys(languageMap)?.map(item => (
-              <Option value={item} >
-                {languageMap[item].label}
-              </Option>
-            ))}
+          {Object.keys(languageMap)?.map((item) => (
+            <Option value={item}>{languageMap[item].label}</Option>
+          ))}
         </Select>
       </LanguageWrapper>
     </LanguageContainer>
