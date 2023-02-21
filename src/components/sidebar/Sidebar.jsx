3a0a52600ce0394391ccef11369485212586f4cb
Delvin Lim <tkjpublic@gmail.com>
2023-02-21 20:08:47 +0700

    Update sidebar

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -13,7 +13,7 @@ const Sidebar = ({ isOpen, toggle }) => {
         <SidebarWrapper>
           <SideBtnWrap>
             <SidebarRoute to="sc_qABwer tOUyTS" onClick={toggle} spy={true} smooth={true} duration={500}>About</SidebarRoute>
-            <SidebarRoute to="sc_qPRwer tOJyECT" onClick={toggle} spy={true} smooth={true} duration={500}>Projects</SidebarRoute>
+            <SidebarRoute to="sc_qPRwer tODyUCT" onClick={toggle} spy={true} smooth={true} duration={500}>Products</SidebarRoute>
             <SidebarRoute to="sc_qCOwer tNTyACT" onClick={toggle} spy={true} smooth={true} duration={500}>Contact</SidebarRoute>
           </SideBtnWrap>
         </SidebarWrapper>
